2ab6fbb193335317b60d146891098f68f155c71e
ArsenZar <arsenzar@gmail.com>
2025-04-05 16:41:14 +0300

    practice-2 (use querySelector '.link' and '.mainList', learn how i can use DOM data)

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
-const elem = document.querySelector(".mainList");
+const elem = document.querySelector(".link");
 
 console.log(elem);
+
+elem.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    console.log(event.target.href);
+})
