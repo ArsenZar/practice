3d76719d2a11e3b7ab32139708c09a1497d83ef4
ArsenZar <arsenzar@gmail.com>
2025-04-05 22:17:53 +0300

    practice-8 (fix 0.0v)

--- js/main.js
+++ js/main.js
@@ -1,31 +1,23 @@
+'use strict';
+
 const elem = document.querySelector(".mainList");
 const secondElem = document.querySelectorAll(".link");
 console.log(secondElem[1]);
 
 
-elem.addEventListener("click", event => {
-    event.preventDefault();
 
-    console.log(event);
 
-    if (event.target.dataset.text === "two") {
-      instance.show();  
-    }else{
-        instance.close();  
-    };
+elem.addEventListener("click", function (event) {
+	event.preventDefault();
 
+	if (event.target.nodeName !== "A") {
+		return;
+	}
+
+	basicLightbox.create(`
+		<img width="1400" height="900" src="https://ireland.apollo.olxcdn.com/v1/files/tqlxwjrycp5t2-UA/image;s=1440x960">
+	`).show();
 });
-
-elem.addEventListener("keydown", event => {
-
-    console.log(event);
-
-});
-
-const instance = basicLightbox.create(`
-    <img src="https://ireland.apollo.olxcdn.com/v1/files/tqlxwjrycp5t2-UA/image;s=1440x960" alt="Description">
-`);
-
 
 
 
